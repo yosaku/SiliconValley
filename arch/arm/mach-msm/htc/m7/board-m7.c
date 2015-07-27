fdebaf7d35a3d9e51d891202adfe2f17ec493a86
pattyboi <patpat1325179@gmail.com>
2015-07-27 12:59:24 -0400

    corrected i2c freqs thx to poondog and zeroblade

--- arch/arm/mach-msm/htc/m7/board-m7.c
+++ arch/arm/mach-msm/htc/m7/board-m7.c
@@ -3743,17 +3743,17 @@ static struct slim_boardinfo apq8064_slim_devices[] = {
 };
 
 static struct msm_i2c_platform_data apq8064_i2c_qup_gsbi1_pdata = {
-	.clk_freq = 400000,
+	.clk_freq = 100000,
 	.src_clk_rate = 24000000,
 };
 
 static struct msm_i2c_platform_data apq8064_i2c_qup_gsbi2_pdata = {
-	.clk_freq = 400000,
+	.clk_freq = 384000,
 	.src_clk_rate = 24000000,
 };
 
 static struct msm_i2c_platform_data apq8064_i2c_qup_gsbi3_pdata = {
-	.clk_freq = 400000,
+	.clk_freq = 384000,
 	.src_clk_rate = 24000000,
 #ifdef CONFIG_SERIAL_CIR
 	.share_uart_flag = 1,
@@ -3761,7 +3761,7 @@ static struct msm_i2c_platform_data apq8064_i2c_qup_gsbi3_pdata = {
 };
 
 static struct msm_i2c_platform_data apq8064_i2c_qup_gsbi4_pdata = {
-	.clk_freq = 400000,
+	.clk_freq = 384000,
 	.src_clk_rate = 24000000,
 	.share_uart_flag = 1,
 };
